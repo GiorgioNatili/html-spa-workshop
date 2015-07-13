95a47ae1542429a2938089d29973c43cf0908536
Jonathan Barton <jonathan.barton@mheducation.com>
2015-07-13 14:18:17 -0400

    removed finished labs-jb

--- lab1/lab1-finished/assets/javascripts/main.js
+++
@@ -1,9 +0,0 @@
-require({
-  paths: {
-    jquery: "vendor/jquery/jquery"
-  }
-}, ["app/example-view"], function(view) {
-  "use strict";
-  view("body");
-});
-
--- lab1/lab1-finished/public/javascripts/app/example-view.js
+++
@@ -1,9 +0,0 @@
-define(["jquery"], function($) {
-  "use strict";
-
-  var render = function(element) {
-    $(element).append("<div class='name'>This is a sample Mimosa web app</div>");
-  };
-
-  return render;
-});
