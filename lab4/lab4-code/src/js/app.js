b2937dcb0a018f6744d28e365f40bed1965e481c
Jonathan Barton <jonathan.barton@mheducation.com>
2015-07-13 19:38:43 -0400

    lab4 starter-jb

---
+++ lab4/lab4-code/src/js/app.js
@@ -0,0 +1,10 @@
+import jquery from 'jquery';
+//TODO: import Fib class
+
+class App {
+	//TODO: add click handler here. Instantiate the Fib generator class
+}
+
+$(function(){
+	//TODO: instantiate App class and click event here
+});
---
+++ lab4/lab4-code/src/js/fib.js
@@ -0,0 +1,5 @@
+class Fib {
+	//TODO: create method to generate random fibonacci number
+}
+
+//TODO: export Fib class
